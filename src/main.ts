a17d545ca444a79a2474dd2b70bd88e7dc9077a8
Nazimjonovna <sturayeva000@gmail.com>
2024-10-29 16:32:37 +0500

    netlify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
+  ConfigModule.forRoot({
+    isGlobal: true, // Makes the config globally available
+  });
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
