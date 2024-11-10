0194827d08edf2ec3ad2acc560979f5cf413800d
Ramz001 <rkenjaev1@gmail.com>
2024-11-10 14:12:06 +0500

    fixing checkPerformTransaction if check bugs

--- src/payme/methods/checkPerformTransaction.ts
+++ src/payme/methods/checkPerformTransaction.ts
@@ -5,7 +5,11 @@ export async function checkPerformTransaction(
   this: any,
   checkPerformTransactionDto: CheckPerformTransactionDto,
 ) {
-  const userId = checkPerformTransactionDto.params?.account?.user_id;
+  const userId = checkPerformTransactionDto.params?.account?.user_id
+    ? Number(checkPerformTransactionDto.params?.account?.user_id)
+    : null;
+  const price = checkPerformTransactionDto.params.amount;
+
   if (!userId) {
     return {
       error: {
@@ -19,38 +23,60 @@ export async function checkPerformTransaction(
     };
   }
 
-  const parsedUserId = Number(userId);
-  if (isNaN(parsedUserId)) {
-    return {
-      error: {
-        code: ErrorStatusCodes.MissingUserId,
-        message: {
-          uz: 'Foydalanuvchi identifikatori noto‘g‘ri',
-          en: 'Invalid user ID',
-          ru: 'Неверный идентификатор пользователя',
-        },
-      },
-    };
-  }
-
-  const price = checkPerformTransactionDto.params.amount / 100;
-
   const balance = await this.prismaService.subscribtion.findUnique({
-    where: { id: parsedUserId },
+    where: { id: userId },
   });
 
-  if (!balance || typeof balance.price !== 'number' || balance.price < price) {
+  console.log(balance, price);
+  console.log(userId);
+  // wrong sum balance: null
+  // wrong sum price: 100
+  // wrong sum selected sum: 100
+
+  // transaction does not exist balance: null
+  // transaction does not exist price: 102
+  // transaction does not selected sum: 100
+
+  if (!balance) {
     return {
       error: {
         code: ErrorStatusCodes.TransactionNotAllowed,
         message: {
-          uz: "Hisobda yetarli mablag' mavjud emas",
-          en: 'Insufficient balance',
-          ru: 'Недостаточно средств на счету',
+          uz: "Foydalanuvchi balansi yo'q",
+          en: 'User balance does not exist',
+          ru: 'Баланс пользователя не существует',
         },
       },
     };
   }
+
+  if (price < 1 || price > 999999999 || price > balance?.price) {
+    return {
+      error: {
+        code: ErrorStatusCodes.InvalidAmount,
+        message: {
+          ru: 'Неверная сумма',
+          uz: 'Incorrect amount',
+          en: 'Incorrect amount',
+        },
+      },
+    };
+  }
+
+  // const parsedUserId = Number(userId);
+
+  // if (isNaN(parsedUserId)) {
+  //   return {
+  //     error: {
+  //       code: ErrorStatusCodes.MissingUserId,
+  //       message: {
+  //         uz: 'Foydalanuvchi identifikatori noto‘g‘ri',
+  //         en: 'Invalid user ID',
+  //         ru: 'Неверный идентификатор пользователя',
+  //       },
+  //     },
+  //   };
+  // }
 
   return { result: { allow: true } };
 }
